9f73ef462fd2edd5fdca459b83aaa723c83eeea4
reyzaherdianti <herdianti.reyza@gmail.com>
2025-05-27 09:49:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ st.write(
 st.image("file_00000000c2b061f7a933c095c260bbda.png", width=200)
 st.image("IMG-20250526-WA0148.jpg", width=200)
 st.title("Aplikasi Sederhana")
-st.header("Aplikasi Mengecek Nilai Genap/Ganji1")
-angka = st.number_input("Tulis sebuah Angka:", values, step-1)
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step-1)
 
-if (angka 2) 0:
-    st.write(f"(angka) adalah Bilangan Genap")
+if (angka % 2) == 0:
+  st.write(f"{angka} adalah Bilangan Genap")
 else:
-    st.write(f"(angka) adalah Bilangan Ganjil")
+  st.write(f"{angka} adalah Bilangan Ganjil")
